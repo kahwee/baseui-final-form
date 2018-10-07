fde352f4db949fe26b7688c78d7f1cc595d9a6e7
KahWee Teng <kahwee@uber.com>
2018-10-06 22:33:56 -0700

    Initial

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = (baseConfig, env, defaultConfig) => {
+  // Extend defaultConfig as you need.
+
+  defaultConfig.module.rules.push({
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  });
+  return defaultConfig;
+};
---
+++ README.md
@@ -0,0 +1,22 @@
+# baseui-final-form
+
+_(Not ready)__
+
+How to use
+
+```javascript
+yarn add baseui react react-dom
+```
+
+## Development
+
+```javascript
+yarn
+yarn storybook
+```
+
+Go to [http://localhost:6006](http://localhost:6006) to view storybook.
+
+## License
+
+MIT
