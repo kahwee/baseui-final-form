595cadf9b13253a84451c5c7fe2db73a8430430d
Liyang Zhang <liyangz@twitter.com>
2018-10-07 14:17:28 -0700

    added toggle

---
+++ src/toggle/index.js
@@ -0,0 +1,18 @@
+// @flow
+import * as React from 'react';
+import {type FieldRenderProps} from 'react-final-form';
+import {FormControl} from 'baseui/form-control';
+import {Checkbox} from 'baseui/checkbox';
+
+export type Props = {
+  caption: string | React.Node,
+  label: string | React.Node,
+} & FieldRenderProps;
+
+export default function renderInput({input, meta, caption, label}: Props) {
+  return (
+    <FormControl label={label} caption={caption} error={meta.errror}>
+      <Checkbox id="test" error {...input} checkmarkType="toggle" />
+    </FormControl>
+  );
+}
---
+++ src/toggle/stories.js
@@ -0,0 +1,33 @@
+/* eslint-env node */
+// @flow
+
+import * as React from 'react';
+import {Form, Field} from 'react-final-form';
+import {storiesOf} from '@storybook/react';
+import withBaseui from '../with-baseui';
+import {Button} from 'baseui/button';
+import Checkbox from './index';
+
+storiesOf('Toggle', module)
+  .addDecorator(withBaseui)
+  .add('Basic', () => (
+    <Form
+      onSubmit={() => {}}
+      validate={() => {}}
+      render={({handleSubmit, pristine, invalid}) => (
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="enabled"
+            component={Checkbox}
+            caption="enabled"
+            label="enabled"
+            type="checkbox"
+          />
+
+          <Button type="submit" disabled={pristine || invalid}>
+            Submit
+          </Button>
+        </form>
+      )}
+    />
+  ));
