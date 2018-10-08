ac4404a3086d5233273906b203b7a746cb970d4f
Liyang Zhang <liyangz@twitter.com>
2018-10-07 19:59:07 -0700

    added conditions

--- src/stories.js
+++ src/stories.js
@@ -6,24 +6,54 @@ import {Form, Field} from 'react-final-form';
 import {storiesOf} from '@storybook/react';
 import withBaseui from './with-baseui';
 import {Button} from 'baseui/button';
-import Checkbox from './checkbox/index';
+import RadioGroup from './radiogroup/index';
+import Input from './input/index';
+
+const Condition = ({when, is, children}) => (
+  <Field name={when} subscription={{value: true}}>
+    {({input: {value}}) => (value === is ? children : null)}
+  </Field>
+);
 
 storiesOf('Playground', module)
   .addDecorator(withBaseui)
   .add('Basic', () => (
+    // Target: City and Zipcode options (radio group)
+    // Input: CityName
+    // Input: ZipCode
     <Form
+      initialValues={{target: 'city'}}
       onSubmit={() => {}}
       validate={() => {}}
       render={({handleSubmit, pristine, invalid}) => (
         <form onSubmit={handleSubmit}>
           <Field
-            name="yesIcan"
-            component={Checkbox}
-            caption="Yes, I can"
+            name="target"
+            component={RadioGroup}
+            caption="Pick a target"
             label="Target"
-            type="checkbox"
+            options={[
+              {label: 'City', value: 'city'},
+              {label: 'Zip Code', value: 'zipcode'},
+            ]}
+            type="select-multiple"
           />
-
+          <Condition when="target" is="zipcode">
+            <Field
+              name="zipcode"
+              component={Input}
+              caption="Enter your zipcode"
+              label="ZipCode"
+            />
+          </Condition>
+          <Condition when="target" is="city">
+            <Field
+              name="city"
+              component={Input}
+              caption="Enter your city"
+              label="City"
+            />
+          </Condition>
           <Button type="submit" disabled={pristine || invalid}>
             Submit
           </Button>
