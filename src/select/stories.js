ac3eb1281fa860ae8b7c7ba204ecdf1e5a39c395
KahWee Teng <kahwee@uber.com>
2018-10-13 20:55:12 -0700

    Add tests for select

--- src/select/stories.js
+++ src/select/stories.js
@@ -3,18 +3,39 @@
 
 import * as React from 'react';
 import {Form, Field} from 'react-final-form';
+import {action} from '@storybook/addon-actions';
 import {storiesOf} from '@storybook/react';
-import withBaseui from '../with-baseui';
 import {Button} from 'baseui/button';
+import withBaseui from '../with-baseui';
 import Select from './index';
 import options from '../native-select/__tests__/__fixtures__/fruit-options.json';
 
 storiesOf('Select', module)
   .addDecorator(withBaseui)
-  .add('Basic', () => (
+  .add('Single', () => (
     <Form
-      onSubmit={() => {}}
-      initialValues={{fruits: '2'}}
+      onSubmit={action('submit')}
+      initialValues={{fruit: 'pineapple'}}
+      render={({handleSubmit, pristine, invalid}) => (
+        <form onSubmit={handleSubmit}>
+          <Button type="submit" disabled={pristine || invalid}>
+            Submit
+          </Button>
+          <Field
+            name="fruit"
+            component={Select}
+            caption="Please select a fruit"
+            label="My fruits"
+            options={options}
+          />
+        </form>
+      )}
+    />
+  ))
+  .add('Multiple', () => (
+    <Form
+      onSubmit={action('submit')}
+      initialValues={{fruits: ['pineapple', 'apple']}}
       render={({handleSubmit, pristine, invalid}) => (
         <form onSubmit={handleSubmit}>
           <Button type="submit" disabled={pristine || invalid}>
@@ -23,10 +44,10 @@ storiesOf('Select', module)
           <Field
             name="fruits"
             component={Select}
-            caption="Please select a fruit"
+            caption="Please select multiple fruits"
             label="My fruits"
             options={options}
-            type="select-multiple"
+            multiple
           />
         </form>
       )}
