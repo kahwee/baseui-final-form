7450347dfab2b8a6de452f498cf23ddec45f6788
KahWee Teng <kahwee@uber.com>
2018-10-13 17:47:03 -0700

    Add tests for validate

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 // @flow
+import * as validate from './validate';
+
 export {default as Input} from './input/index';
+export {default as Checkbox} from './checkbox/index';
+export {default as RadioGroup} from './radiogroup/index';
+export {default as NativeSelect} from './native-select/index';
+export {default as Select} from './select/index';
+export {default as Toggle} from './toggle/index';
+export {default as Textarea} from './textarea/index';
+export {validate};
