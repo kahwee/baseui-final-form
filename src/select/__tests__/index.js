bda81fe99fd5498e23ce134b44bd800e90a6de5e
KahWee Teng <kahwee@uber.com>
2018-10-13 20:55:12 -0700

    Add tests for select

---
+++ src/select/__tests__/index.js
@@ -0,0 +1,62 @@
+// @flow
+//import './global-browser.mock';
+import * as React from 'react';
+import {render, cleanup} from 'react-testing-library';
+import {Form, Field} from 'react-final-form';
+import Select from '../index';
+import BaseuiProvider from '../../with-baseui';
+import options from '../../native-select/__tests__/__fixtures__/fruit-options.json';
+
+describe('select', () => {
+  const defaultProps = {
+    name: 'fruit',
+    label: 'My favorite fruit',
+    options,
+    component: Select,
+  };
+
+  afterEach(cleanup);
+
+  it('should pass sanity checks for single select', async () => {
+    const {container, getByText} = render(
+      <BaseuiProvider>
+        <Form onSubmit={() => {}} initialValues={{fruit: 'pineapple'}}>
+          {({handleSubmit}) => (
+            <form onSubmit={handleSubmit}>
+              <Field
+                {...defaultProps}
+                inputProps={{className: 'select-test-for-jest-1'}}
+              />
+            </form>
+          )}
+        </Form>
+      </BaseuiProvider>
+    );
+    expect(container.querySelector('.select-test-for-jest-1')).toBeDefined();
+    expect(getByText('Pineapple')).toBeDefined();
+  });
+
+  it('should pass sanity checks for multiple select', async () => {
+    const {container, getByText} = render(
+      <BaseuiProvider>
+        <Form
+          onSubmit={() => {}}
+          initialValues={{fruit: ['pineapple', 'apple']}}
+        >
+          {({handleSubmit}) => (
+            <form onSubmit={handleSubmit}>
+              <Field
+                {...defaultProps}
+                multiple
+                inputProps={{className: 'select-test-for-jest-2'}}
+              />
+            </form>
+          )}
+        </Form>
+      </BaseuiProvider>
+    );
+    expect(container.querySelector('.select-test-for-jest-2')).toBeDefined();
+    expect(getByText('Pineapple')).toBeDefined();
+    expect(getByText('Apple')).toBeDefined();
+  });
+});
--- src/select/index.js
+++ src/select/index.js
@@ -4,16 +4,49 @@ import {type MultipleValuesFieldRenderProps} from '../types.js.flow';
 import {FormControl} from 'baseui/form-control';
 import {Select} from 'baseui/select';
 
+type Props = {
+  multiple?: boolean,
+} & MultipleValuesFieldRenderProps;
 export default function render({
   input,
+  inputProps,
   meta,
   caption,
+  multiple,
   label,
   options,
-}: MultipleValuesFieldRenderProps) {
+}: Props) {
+  const {value, onChange, ...inputRest} = input;
+  let selectedOptions;
+  if (multiple) {
+    selectedOptions = options.filter(option => value.includes(option.id));
+  } else {
+    selectedOptions = options.filter(option => option.id === value);
+  }
   return (
     <FormControl label={label} caption={caption} error={meta.error}>
-      <Select {...input} options={options} />
+      <Select
+        {...inputRest}
+        {...inputProps}
+        multiple={multiple}
+        selectedOptions={selectedOptions}
+        onChange={(ev, params) => {
+          if (multiple) {
+            onChange(
+              Array.isArray(params.selectedOptions)
+                ? params.selectedOptions.map(option => option.id)
+                : undefined
+            );
+          } else {
+            onChange(
+              Array.isArray(params.selectedOptions) && params.selectedOptions[0]
+                ? params.selectedOptions[0].id
+                : undefined
+            );
+          }
+        }}
+        options={options}
+      />
     </FormControl>
   );
 }
