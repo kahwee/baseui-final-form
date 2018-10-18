f7fc219e78fa63fb5bcd11b61304172fe754d4d3
KahWee Teng <tengkahwee@gmail.com>
2018-10-18 01:16:10 -0700

    Add checkbox-group

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // @flow
 import * as validate from './validate';
 
-export {default as Input} from './input/index';
 export {default as Checkbox} from './checkbox/index';
-export {default as RadioGroup} from './radiogroup/index';
+export {default as CheckboxGroup} from './checkbox-group/index';
+export {default as Input} from './input/index';
 export {default as NativeSelect} from './native-select/index';
+export {default as RadioGroup} from './radio-group/index';
 export {default as Select} from './select/index';
-export {default as Toggle} from './toggle/index';
 export {default as Textarea} from './textarea/index';
+export {default as Toggle} from './toggle/index';
 export {validate};
