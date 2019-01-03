58490df8179c7a6e2c326b175ab59f764566d5cd
KahWee Teng <kahwee@uber.com>
2019-01-02 17:14:33 -0800

    Added more advanced Select example

--- src/select/stories.js
+++ src/select/stories.js
@@ -3,12 +3,35 @@
 import * as React from 'react';
 import {Button} from 'baseui/button';
 import Select from './index';
+import SelectReadme from './README.md';
 import {action} from '@storybook/addon-actions';
 import options from '../native-select/__tests__/__fixtures__/fruit-options.json';
 import {storiesOf} from '@storybook/react';
+import {styled} from 'baseui';
+import {withReadme} from 'storybook-readme';
 import {Field, Form} from 'react-final-form';
 
+const CustomValueWrapper = styled('span', {
+  display: 'inline-flex',
+  alignItems: 'center',
+});
+
+function CustomOptionLabel({
+  option,
+}: {
+  // eslint-disable-next-line flowtype/no-weak-types
+  option: any,
+}) {
+  return (
+    <CustomValueWrapper>
+      {option.id}
+      {` (${option.emoji})`}
+    </CustomValueWrapper>
+  );
+}
+
 storiesOf('Select', module)
+  .addDecorator(withReadme(SelectReadme))
   .add('Single', () => (
     <Form
       onSubmit={action('submit')}
@@ -43,6 +66,41 @@ storiesOf('Select', module)
             label="My fruits"
             options={options}
             multi
+          />
+          <Button type="submit" disabled={pristine || invalid}>
+            Submit
+          </Button>
+        </form>
+      )}
+    />
+  ))
+  .add('With overrides and custom components', () => (
+    <Form
+      onSubmit={action('submit')}
+      initialValues={{fruits: ['pineapple', 'apple']}}
+      render={({handleSubmit, pristine, invalid}) => (
+        <form onSubmit={handleSubmit}>
+          Notice that the dropdown list items now have a green hover effect:
+          <Field
+            name="fruits"
+            component={Select}
+            caption="Please select multiple fruits"
+            label="My fruits"
+            options={options}
+            multi
+            getOptionLabel={({option}) => <CustomOptionLabel option={option} />}
+            overrides={{
+              DropdownListItem: {
+                style: ({$theme, $isHighlighted}) => ({
+                  backgroundColor: $isHighlighted
+                    ? $theme.colors.positive50
+                    : 'transparent',
+                  ':hover': {
+                    backgroundColor: $theme.colors.positive50,
+                  },
+                }),
+              },
+            }}
           />
           <Button type="submit" disabled={pristine || invalid}>
             Submit
