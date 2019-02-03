d2e186003b8fdbe6136cfdb6b5c11de8581ece3a
KahWee Teng <kahwee@uber.com>
2019-02-02 23:49:25 -0800

    Update .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  parser: 'babylon',
+};
