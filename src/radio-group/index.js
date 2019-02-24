2e0a88d48ebbaad4cff8556815a1030a2581e553
KahWee Teng <tengkahwee@gmail.com>
2019-02-23 22:26:38 -0800

    RadioGroup API has changed in Baseui, need to use Radio instead of StyledRadio now

--- src/radio-group/index.js
+++ src/radio-group/index.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import {type FieldRenderProps} from '../types.js.flow';
 import {FormControl} from 'baseui/form-control';
 import assignProps from '../util/assign-props';
-import {RadioGroup, StyledRadio} from 'baseui/radio';
+import {Radio, RadioGroup} from 'baseui/radio';
 
 export default function render(props: FieldRenderProps) {
   const {formControlProps, inputProps, options, onChange} = assignProps(props);
@@ -20,9 +20,9 @@ export default function render(props: FieldRenderProps) {
       >
         {options.map((option, index) => {
           return (
-            <StyledRadio value={option.id} {...option} key={index}>
+            <Radio value={option.id} {...option} key={index}>
               {option.label}
-            </StyledRadio>
+            </Radio>
           );
         })}
       </RadioGroup>
