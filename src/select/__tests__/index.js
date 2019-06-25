0e3b653ef4b90ad45cc5cc76ed858e37ebe09e96
KahWee Teng <kahwee@uber.com>
2019-06-25 08:48:08 -0700

    Make sure onChange only fires once in baseui select

--- src/select/__tests__/index.js
+++ src/select/__tests__/index.js
@@ -1,7 +1,7 @@
 // @flow
 import * as React from 'react';
-import {Field, Form} from 'react-final-form';
-import {cleanup, render} from '@testing-library/react';
+import {Field, Form, FormSpy} from 'react-final-form';
+import {cleanup, fireEvent, render} from '@testing-library/react';
 import BaseuiProvider from '../../with-baseui';
 import Select from '../index';
 import options from '../../native-select/__tests__/__fixtures__/fruit-options.json';
@@ -55,6 +55,7 @@ describe('select', () => {
   });
 
   it('should pass sanity checks for multiple select', () => {
+    const onChange = jest.fn();
     const {container, getByText} = render(
       <BaseuiProvider>
         <Form
@@ -63,18 +64,34 @@ describe('select', () => {
         >
           {({handleSubmit}) => (
             <form onSubmit={handleSubmit}>
-              <Field
-                {...defaultProps}
-                multi
-                className="select-test-for-jest-2"
-              />
+              <FormSpy onChange={onChange} subscription={{values: true}} />
+              <Field {...defaultProps} multi />
             </form>
           )}
         </Form>
       </BaseuiProvider>
     );
-    expect(container.querySelector('.select-test-for-jest-2')).toBeDefined();
+    const baseuiSelect = container.querySelector('[data-baseweb=select]');
+    expect(baseuiSelect).toBeDefined();
+    const [tagApple, tagPineapple] = container.querySelectorAll(
+      '[data-baseweb=tag]'
+    );
     expect(getByText('Pineapple')).toBeDefined();
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange).toHaveBeenNthCalledWith(1, {
+      values: {fruit: expect.arrayContaining(['pineapple', 'apple'])},
+    });
+    expect(tagPineapple).toBeDefined();
     expect(getByText('Apple')).toBeDefined();
+    fireEvent.click(tagApple.querySelector('svg'));
+    expect(onChange).toHaveBeenCalledTimes(2);
+    expect(onChange).toHaveBeenNthCalledWith(2, {
+      // should not contain apple any more since removal
+      values: {fruit: expect.not.arrayContaining(['apple'])},
+    });
+    expect(getByText('Pineapple')).toBeDefined();
+    expect(() => {
+      getByText('Apple');
+    }).toThrow();
   });
 });
