771d976c06cda2b7f0845c310e4715e642e0069f
KahWee Teng <kahwee@uber.com>
2019-09-19 11:28:57 -0700

    Extra checks to make sure multi select can a value that is an array

--- src/setup-tests.js
+++ src/setup-tests.js
@@ -1,3 +1,15 @@
 /* eslint-env node */
 // @noflow
 import 'jest-extended';
+
+// Shimming for popover
+if (window.document) {
+  window.document.createRange = () => ({
+    setStart: () => {},
+    setEnd: () => {},
+    commonAncestorContainer: {
+      nodeName: 'BODY',
+      ownerDocument: document,
+    },
+  });
+}
