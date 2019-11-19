f297d409c5539503a0f3ee367c3ed445836e607f
KahWee Teng <kahwee@uber.com>
2019-11-19 15:13:45 -0800

    (fix) Flow fixes

--- src/select/unique-concat.js
+++ src/select/unique-concat.js
@@ -1,10 +1,10 @@
 // @flow
 
 export function uniqueConcat<T>(
-  arr1: Array<T>,
-  arr2: Array<T>,
+  arr1: $ReadOnlyArray<T>,
+  arr2: $ReadOnlyArray<T>,
   idKey: string = 'id'
-): Array<T> {
+): $ReadOnlyArray<T> {
   return arr1.concat<T, T>(
     arr2.reduce<T>((valA, valB) => {
       // $FlowFixMe
