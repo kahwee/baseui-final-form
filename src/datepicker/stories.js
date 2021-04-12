469bc333a6aa7b9a40c3e13b0d8cecb22412a7de
Luke Brennan <luke.brennan@uber.com>
2021-04-12 15:54:26 -0700

    Add adaptToTimePicker

--- src/datepicker/stories.js
+++ src/datepicker/stories.js
@@ -6,9 +6,14 @@ import {Button} from 'baseui/button';
 import {Datepicker, formatDate} from 'baseui/datepicker';
 import {Field, Form} from 'react-final-form';
 import {FormControl} from 'baseui/form-control';
+import {TimePicker} from 'baseui/timepicker';
 import {action} from '@storybook/addon-actions';
 import {adaptToFormControl} from '../form-control';
-import {adaptToRangeDatepicker, adaptToSingleDatepicker} from './adapters';
+import {
+  adaptToRangeDatepicker,
+  adaptToSingleDatepicker,
+  adaptToTimePicker,
+} from './adapters';
 import {storiesOf} from '@storybook/react';
 
 function formatDateAtIndex(dates: ?Date | ?Array<Date>, index: number) {
@@ -28,9 +33,9 @@ storiesOf('Datepicker', module)
           <Field
             name="birthday"
             label="Birthday"
-            format={(val) => (val ? new Date(val) : new Date())}
-            parse={(val) => val.toISOString()}
-            render={(props) => (
+            format={val => (val ? new Date(val) : new Date())}
+            parse={val => val.toISOString()}
+            render={props => (
               <FormControl {...adaptToFormControl(props)}>
                 <Datepicker {...adaptToSingleDatepicker(props)} />
               </FormControl>
@@ -53,11 +58,11 @@ storiesOf('Datepicker', module)
           <Field
             name="vacation"
             label="Vacation"
-            format={(value) =>
-              (value || []).map((val) => (val ? new Date(val) : new Date()))
+            format={value =>
+              (value || []).map(val => (val ? new Date(val) : new Date()))
             }
-            parse={(value) => (value || []).map((val) => val.toISOString())}
-            render={(props) => (
+            parse={value => (value || []).map(val => val.toISOString())}
+            render={props => (
               <FormControl {...adaptToFormControl(props)}>
                 <Datepicker {...adaptToRangeDatepicker(props)} />
               </FormControl>
@@ -81,17 +86,42 @@ storiesOf('Datepicker', module)
             name="vacation"
             label="Start Date"
             caption="YYYY/MM/DD"
-            format={(value) =>
-              (value || []).map((val) => (val ? new Date(val) : new Date()))
+            format={value =>
+              (value || []).map(val => (val ? new Date(val) : new Date()))
             }
-            parse={(value) => (value || []).map((val) => val.toISOString())}
-            render={(props) => (
+            parse={value => (value || []).map(val => val.toISOString())}
+            render={props => (
               <FormControl {...adaptToFormControl(props)}>
                 <Datepicker
                   {...adaptToRangeDatepicker(props)}
-                  formatDisplayValue={(date) => formatDateAtIndex(date, 0)}
+                  formatDisplayValue={date => formatDateAtIndex(date, 0)}
                   timeSelectStart
                 />
+              </FormControl>
+            )}
+            onChange={action('vacation changed')}
+          />
+          <Button type="submit" disabled={pristine || invalid}>
+            Submit
+          </Button>
+        </form>
+      )}
+    />
+  ))
+  .add('Time Picker', () => (
+    <Form
+      onSubmit={action('submit')}
+      initialValues={{appointment: '2021-04-01T00:16:22.599Z'}}
+      render={({handleSubmit, pristine, invalid}) => (
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="appointment"
+            label="Appointment"
+            format={value => (value ? new Date(value) : new Date())}
+            parse={avlue => avlue.toISOString()}
+            render={props => (
+              <FormControl {...adaptToFormControl(props)}>
+                <TimePicker {...adaptToTimePicker(props)} />
               </FormControl>
             )}
             onChange={action('vacation changed')}
